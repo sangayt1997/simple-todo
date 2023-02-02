6ef428d1c12ebf78b6f891aed899f6f9f89d89be
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-02 17:20:10 +0600

    remove material tailwind html and replace with material tail wind react

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { Button, ThemeProvider } from "@material-tailwind/react";
+
 function App() {
-  return (
-    <div className="flex justify-center items-center flex-col h-screen">
-      <p>Simple Todo app in react</p>
-        <p className="text-3xl font-bold underline">
-            Hello world!
-        </p>
-        <button
-            className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            data-ripple-light="true"
-        >
-            Button
-        </button>
-    </div>
-  );
+    return (
+        <React.StrictMode>
+            <ThemeProvider>
+                <div className="flex justify-center items-center flex-col h-screen">
+                    <p>Simple Todo app in react</p>
+                    <p className="text-3xl font-bold underline">
+                        Hello world!
+                    </p>
+                    <Button>Button</Button>
+                </div>
+            </ThemeProvider>
+        </React.StrictMode>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-const withMT = require("@material-tailwind/html/utils/withMT");
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
