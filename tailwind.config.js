a16aa632b3ecf085349106faa1a57db63defd418
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-02 16:59:41 +0600

    add material tailwind packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT({
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
