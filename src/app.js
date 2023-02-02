ed81511bb828b642d40b8435c868a7b54669aec3
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-02 17:59:10 +0600

    refactor theming configuration[

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Button, ThemeProvider } from "@material-tailwind/react";
+import { theme } from "./theme";
 
 function App() {
     return (
         <React.StrictMode>
-            <ThemeProvider>
+            <ThemeProvider value={theme}>
                 <div className="flex justify-center items-center flex-col h-screen">
                     <p>Simple Todo app in react</p>
                     <p className="text-3xl font-bold underline">
                         Hello world!
                     </p>
-                    <Button>Button</Button>
+                    <Button color="sky-500">Button</Button>
+                    <Button variant="outlined">outlined</Button>
                 </div>
             </ThemeProvider>
         </React.StrictMode>
